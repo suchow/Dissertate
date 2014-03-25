82208ca0a6fbab0c567fc35f22f7007281f7c74d
Jordan Suchow <suchow@post.harvard.edu>
2014-03-24 23:55:22 -0400

    Add build and source folders

---
+++ src/template.py
@@ -0,0 +1,27 @@
+from docx import Document
+from docx.shared import Inches
+
+document = Document()
+
+document.add_heading('Document Title', 0)
+
+p = document.add_paragraph('A plain paragraph having some ')
+p.add_run('bold').bold = True
+p.add_run(' and some ')
+p.add_run('italic.').italic = True
+
+document.add_heading('Heading, level 1', level=1)
+document.add_paragraph('Intense quote', style='IntenseQuote')
+
+document.add_paragraph(
+    'first item in unordered list', style='ListBullet'
+)
+document.add_paragraph(
+    'first item in ordered list', style='ListNumber'
+)
+
+#document.add_picture('monty-truth.png', width=Inches(1.25))
+
+document.add_page_break()
+
+document.save('demo.docx')
